6fd05bc7c43cc8ad94435f46cb2613747619247f
Željko Beratovic <65452618+zeljkoberatovic@users.noreply.github.com>
2024-04-24 23:31:54 +0200

    local query scopes

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Book extends Model
 {
@@ -12,6 +13,13 @@ class Book extends Model
     public function reviews() {
 
         return $this->hasMany(Review::class);//jedna knjiga moze imati vise recenzija
+
+    }
+
+    //lokalni opseg upita
+    public function scopeTitle(Builder $query, string $title): Builder {
+
+        return $query->where('title', 'LIKE', '%' . $TITLE . '%');
 
     }
 }
