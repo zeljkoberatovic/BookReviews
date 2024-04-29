bcc4feb05799df2830d4284ec38a643260c75670
Željko Beratovic <65452618+zeljkoberatovic@users.noreply.github.com>
2024-04-30 00:41:06 +0200

    cache and cache queries

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -25,9 +25,15 @@ class BookController extends Controller
             'highest_rated_last_6months' => $books->highestRatedLast6Months(),
             default => $books->latest()
         };
-        $books = $books->get();
+
+        //$books = $books->get();
+
+        //kljuc predmemorije
+        $cacheKey = 'books:' . $filter . ':' . $title;
+        $books = cache()->remember($cacheKey, 3600, fn() => $books->get());
 
         return view('books.index', ['books' => $books]);
+        
     }
 
     /**
