fbaa7ade04aef200b4a4a68567a3cf929ba5af4b
Željko Beratovic <65452618+zeljkoberatovic@users.noreply.github.com>
2024-05-01 23:37:41 +0200

    blade component-starRating component

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -43,6 +43,7 @@
                         <div>
                         <div class="book-rating">
                             {{ number_format($book->reviews_avg_rating, 1) }}
+                            <x-star-rating :rating="$book->reviews_avg_rating" />   
                         </div>
                         <div class="book-review-count">
                             out of {{ $book->reviews_count }} {{ Str::plural('reviews', $book->reviews_count)}}
